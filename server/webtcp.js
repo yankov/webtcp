8a3a989a1c9ab0979793b96790027929503f13e0
Artem Yankov <artem@badgeville.com>
2012-10-12 00:09:50 -0700

    forgot main server file

---
+++ server/webtcp.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var sockjs = require('sockjs');
+var Socket = require('./socket.js').Socket;
+var httpClient = require('./httpClient.js').httpClient;
+
+module.exports.WebTCP = function() {
+  this.sockjsServer = sockjs.createServer();
+  this.websockets = {};
+  this.httpClients = {};
+
+  var self = this;
+
+  this.sockjsServer.on('connection', function(conn) {
+    conn.on('data', function(message) {
+      self.dispatch.call(self, conn, message);
+    });
+    
+    conn.on('close', function() {});
+  });
+
+  // client's message processor
+  // TODO: dry out
+  this.dispatch = function(conn, message) {
+    message = JSON.parse(message);
+
+    if (message.type == "tcp") {
+      // create socket binding if it doesn't exist
+      if (!this.websockets[message.sID]) 
+        this.websockets[message.sID] = new Socket(conn, message.sID, message.host, message.port, message.options);
+
+      this.websockets[message.sID].onClientData(message);
+
+    } else if (message.type == "http") {
+        
+      if (!this.httpClients[message.cID]) 
+        this.httpClients[message.cID] = new httpClient(conn, message.cID)
+
+      this.httpClients[message.cID].onClientData(message);
+    }
+  }
+
+  this.listen = (function(that) {
+    return function(port, host) {
+      this.server = http.createServer();
+      this.sockjsServer.installHandlers(this.server, { prefix: '/bridge' });
+      this.server.listen(port, host);
+    }
+  }(this))
+  
+}
