0e5347d85d2b95e8ad32936f8a6939a7701d6ea8
Artem Yankov <artem@badgeville.com>
2012-10-11 23:21:19 -0700

    add http client: handle GET requests

---
+++ server/httpClient.js
@@ -0,0 +1,49 @@
+module.exports.httpClient = function(sockjsConn, cID) {
+  var EVENTS = ["connect", "data", "end", "close", "upgrade", "continue"];
+
+  this.sockjsConn = sockjsConn;
+  this.cID = cID;
+
+  this.createPacket = function() { 
+    return {
+      cID: this.cID,
+      eventName: null,
+      data: null
+    }
+  }
+
+  // how to refactor this?
+  this.get = function(data){
+    http.get(data, (function(that, data) {
+      return function(res) {
+        res.setEncoding(data.encoding || 'utf8');
+
+        res.on('data', (function(that) {
+          return function(data) {
+            that.emitClientEvent("data", data);    
+          }
+        }(that)));        
+        
+    }}(this, data))).on('error', (function(that) { 
+      return function(e) {
+        that.emitClientEvent("data", {error: e});    
+    }}(this)));
+  }
+
+  this.onClientData = (function(that) { return function(data) {
+      if (data.command && data.command == "httpGet") {
+        that.get.call(that, data.args);
+      } else if (data.command && data.command == "httpPost") {
+        that.post.call(that, data);  
+      }
+    }})(this);
+
+
+  // trigger a socket's event on a client side
+  this.emitClientEvent = function(eventName, data) {
+    var pck = this.createPacket();
+    pck.eventName = eventName;
+    pck.data = data;
+    this.sockjsConn.write( JSON.stringify(pck) );     
+  }
+}
---
+++ server/socket.js
@@ -0,0 +1,102 @@
+module.exports.Socket = function(sockjsConn, remoteSocketId, host, port, options) {
+  var EVENTS = ["connect", "data", "end", "close", "timeout", "drain", "error"];
+  
+  this.sockjsConn = sockjsConn;
+  this.remoteSocketId = remoteSocketId;
+  this.remoteAddress = host;
+  this.remotePort = port;
+
+
+  this.options = {
+    //Makes the 'data' event emit a string instead of a Buffer.
+    //Can be 'utf8', 'utf16le' ('ucs2'), 'ascii', or 'hex'
+    encoding: options.encoding || 'utf8', 
+
+    //Sets the socket to timeout after timeout milliseconds of inactivity on the socket
+    timeout: options.timeout || 0,
+
+    //Disables the Nagle algorithm
+    noDelay: options.noDelay || true, 
+
+    keepAlive: options.keepAlive || false, 
+
+    //Set the delay between the last data packet received and the first keepalive probe
+    initialDelay: options.initialDelay || 0
+  }
+
+  this.createPacket = function() { 
+    return {
+      sID: this.remoteSocketId,
+      eventName: null,
+      data: null
+    }
+  }
+
+  this.setOptions = function() {
+    this.client.setEncoding(this.options.encoding);
+    this.client.setTimeout(this.options.timeout);
+    this.client.setNoDelay(this.options.noDelay);
+    this.client.setKeepAlive(this.options.keepAlive, this.options.initialDelay);
+  }
+
+  this.client = new net.Socket();
+  this.client.connect(this.remotePort, this.remoteAddress, (function(that) {
+    return function() { that.setOptions.call(that) }
+  }(this)));
+
+
+  this.mapEvent = function(eventName) {
+    this.client.on(eventName, (function(that) { return function(data){
+      if(data) data = data.toString();
+      that.emitSockEvent(eventName, data); 
+
+      // also delete socket object if one of these events happened
+      if (["end", "close", "timeout"].indexOf(eventName) != -1) 
+        delete websockets[that.remoteSocketId];
+
+    }})(this));
+  }
+
+  //map sock events to client's sock events
+  this.mapEvents = function(events) {
+    for(i in events) 
+      this.mapEvent.call(this, events[i]);
+  }
+
+  this.mapEvents.call(this, EVENTS);
+
+  // trigger a socket's event on a client side
+  this.emitSockEvent = function(eventName, data) {
+    var pck = this.createPacket();
+    pck.eventName = eventName;
+    pck.data = data;
+    this.sockjsConn.write( JSON.stringify(pck) );     
+  }
+
+  this.sendSockOpts = function() {
+    var sockOpts = {
+      _pendingWriteReqs: this.client._pendingWriteReqs,
+      _connectQueueSize: this.client._connectQueueSize,
+      destroyed:         this.client.destroyed,
+      errorEmitted:      this.client.errorEmitted,
+      bytesRead:         this.client.bytesRead,
+      bytesWritten:      this.client.bytesWritten,
+      allowHalfOpen:     this.client.allowHalfOpen,
+      _connecting:       this.client._connecting,
+      writable:          this.client.writable,
+      readable:          this.client.readable,
+    }
+
+    this.emitSockEvent("SockOptsRcv", sockOpts); 
+  }
+
+  // when message comes from a browser client 
+  // write it to socket
+  this.onClientData = (function(that) { return function(data) {
+      if (data.command && data.command == "getSockOpts") {
+        that.sendSockOpts.call(that);
+      } else {
+        that.client.write(data.data);  
+      }
+    }})(this);
+}
