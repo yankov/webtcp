aaf7412afccafa8e8629fc1b099c05d683808fb6
Artem Yankov <artem@badgeville.com>
2012-10-12 19:28:53 -0700

    client events

---
+++ server/client_events.js
@@ -0,0 +1,17 @@
+module.exports.clientEvents = function() {
+  // trigger an event on a client side
+  this.emitClientEvent = function(eventName, data) {
+    var pck = this.createPacket();
+    pck.eventName = eventName;
+    pck.data = data;
+    this.sockjsConn.write( JSON.stringify(pck) );     
+  }
+
+  // process messages comming from a client
+  this.onClientData = function(data) {
+    if (data.command && typeof this[data.command] == "function") 
+      this[data.command](data['args']);
+    else 
+      this.client.write(data.data);  
+  };
+}
